d24879042843289e8a99328944c04cb0f54d54c6
wm0709 <79418414+wm0709@users.noreply.github.com>
2021-04-25 18:03:28 -0500

    V0

---
+++ Exercise 2/app/src/main/java/com/example/duckhunting/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.duckhunting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // get status bar height
+        Resources res = getResources();
+        int statusBarHeight = 0;
+        int statusBarId = res.getIdentifier("status_bar_height", "dimen", "android");
+        if(statusBarId > 0)
+            statusBarHeight = res.getDimensionPixelSize(statusBarId);
+
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        gameView = new GameView(this, size.x, size.y - statusBarHeight);
+        setContentView(gameView);
+    }
+}
